2e39126e4695910826251472a1cc1dd44d86fc56
Daniel Robert <Daniel.Robert.S-S@hotmail.com>
2020-09-23 16:34:55 +1000

    extra operators for float coord

--- Coord/fcoord.cs
+++ Coord/fcoord.cs
@@ -11,8 +11,25 @@ namespace Coord
             x = xi;
             y = yi;
         }
+        public fcoord(coord p)
+        {
+            x = p.x;
+            y = p.y;
+        }
         public float x;
         public float y;
+
+        public static fcoord operator +(fcoord a, fcoord b)
+            => new fcoord(a.x + b.x, a.y + b.y);
+
+        public static fcoord operator +(fcoord a, coord b)
+            => a + new fcoord(b);
+
+        public static fcoord operator -(fcoord a)
+            => new fcoord(-a.x, - a.y);
+
+        public static fcoord operator -(fcoord a, fcoord b)
+            => a + (-b);
 
         public static fcoord operator *(fcoord a, float b)
             => new fcoord(a.x * b, a.y * b);
