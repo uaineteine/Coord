2df265915e6a34c12e847f2a7b5fbf72f027387f
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2023-03-16 13:33:57 +1100

    added in ordinate base class

--- Coord/coord.cs
+++ Coord/coord.cs
@@ -4,24 +4,25 @@ namespace Uaine.Coord
 {
     public class coord
     {
-        public coord(int xi, int yi)
+        public coord(float xi, float yi)
         {
             x = xi;
             y = yi;
         }
-        public coord(coord b)
+        public coord(coord p)
         {
-            x = b.x;
-            y = b.y;
+            x = p.x;
+            y = p.y;
         }
-        public coord(int xandy)
+        public coord(float xandy)
         {
             x = xandy;
             y = xandy;
         }
+        public float x;
+        public float y;
 
-        public int x;
-        public int y;
+        //0 is origin, 1 to 4 are coord quadrants, expecting something not on an axis
         public int quadrant()
         {
             //return quad coord is in
@@ -29,63 +30,53 @@ namespace Uaine.Coord
             if (x > 0) return (y >= 0) ? 1 : 4;
             return (y >= 0) ? 2 : 3;
         }
+
         public double angle()
         {
-            float v = x/y;
+            float v = x / y;
             return Math.Atan(v);
         }
-        public int Mag()
+
+        public float Mag()
         {
             return x * y;
         }
+
         public double Norm()
         {
-            fcoord a = new fcoord(this);
-            return a.Norm();
+            double xd = (double)x;
+            double yd = (double)y;
+            return Math.Sqrt(xd*xd + yd*yd);
         }
+
         public double DistanceFrom(coord a)
         {
             coord d = this - a;
             return d.Norm();
         }
-        public fcoord toFloat()
-        {
-            return new fcoord(this);
-        }
-        public fcoord floatdiv(coord b)
-        {
-            return new fcoord((float)x / (float)b.x, (float)y / (float)b.y);
-        }
-        /*
-        public void rationalise()
-        {
-            
-        }
-        */
+        public static fcoord operator +(coord a, coord b)
+            => new fcoord(a.x + b.x, a.y + b.y);
 
         public static coord operator +(coord a, coord b)
-            => new coord(a.x + b.x, a.y + b.y);
+            => a + new coord(b);
 
-        public static coord operator +(coord a, int b)
-            => new coord(a.x + b, a.y + b);
-
-        public static coord operator -(coord a, int b)
-            => new coord(a.x - b, a.y - b);
+        public static coord operator -(coord a)
+            => new coord(-a.x, - a.y);
 
         public static coord operator -(coord a, coord b)
-            => new coord(a.x - b.x, a.y - b.y);
+            => a + (-b);
 
-        public static coord operator /(coord a, coord b)
-            => new coord(a.x / b.x, a.y / b.y);
-
-        public static coord operator /(coord a, int b)
-            => new coord(a.x / b, a.y / b);
+        public static coord operator *(coord a, float b)
+            => new coord(a.x * b, a.y * b);
 
         public static coord operator *(coord a, coord b)
             => new coord(a.x * b.x, a.y * b.y);
 
-        public static coord operator *(coord a, int b)
-            => new coord(a.x * b, a.y * b);
+        public static coord operator /(coord a, float b)
+            => new coord(a.x / b, a.y / b);
+
+        public static coord operator /(coord a, coord b)
+            => new coord(a.x / b.x, a.y / b.y);
 
         public static readonly coord zero = new coord(0, 0);
         public static readonly coord one = new coord(1, 1); //value of 1,1
---
+++ Coord/ordinate.cs
@@ -0,0 +1,18 @@
+﻿namespace Uaine.Coord
+{
+    public class Ordinate<T>
+    {
+        private T value;
+
+        public Ordinate(T value)
+        {
+            this.value = value;
+        }
+        public T Value
+        {
+            get { return value; }
+            set { this.value = value; }
+        }
+    }
+
+}
