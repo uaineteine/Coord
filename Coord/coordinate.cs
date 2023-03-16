de0d4f6eec27f2ae1db073f44ab6afc59fa498b0
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2023-03-16 13:47:52 +1100

    create coordinate class to use ordinate base

---
+++ Coord/coordinate.cs
@@ -0,0 +1,34 @@
+﻿namespace Uaine.Coord
+{
+    public class Coordinate<T>
+    {
+        private Ordinate<T> x;
+        private Ordinate<T> y;
+        private Ordinate<T> z;
+
+        public Coordinate(Ordinate<T> x, Ordinate<T> y, Ordinate<T> z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public Ordinate<T> X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public Ordinate<T> Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public Ordinate<T> Z
+        {
+            get { return z; }
+            set { z = value; }
+        }
+    }
+}
